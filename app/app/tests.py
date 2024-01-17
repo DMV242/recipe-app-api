0d5509d99ab819a41ac495bc7df91660beafa5e6
MMEDV242 <mvouladavid@gmail.com>
2024-01-17 09:20:00 +0100

    implement simple test case

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    "Test calc module"
+    
+    def test_add_numbers(self):
+        """Test add numbers function with positive numbers"""
+        
+        res = calc.add(5,6)
+        
+        self.assertEqual(11,res)
