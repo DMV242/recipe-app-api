d26dd2789993b62959d7f65aace1ff2ce4ffb227
MMEDV242 <mvouladavid@gmail.com>
2024-01-17 15:25:26 +0100

    add psycopg2 to requirements , install postgres service on docker and init test with django

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 def add(x,y):
     "Add x and y and return result"
     return x+y
+
+def substract(x,y):
+    "substract x and y and return result"
+    return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -13,3 +13,10 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
         
         self.assertEqual(11, res)
+        
+    
+    def test_substract_numbers(self):
+        """Test substracting numbers"""
+        res = calc.substract(10,15)
+    
+        self.assertEqual(res,5)
