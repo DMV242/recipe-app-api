ebdc85dc1db86586a2a8c4825fd72c3c748bf986
MMEDV242 <mvouladavid@gmail.com>
2024-01-19 20:50:25 +0100

    Add Django user model and manager.

---
+++ app/core/tests/tests_models.py
@@ -0,0 +1,35 @@
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models """
+
+    def test_create_user_with_successful(self):
+        email = "test@example.com"
+        password = "testpass1234"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_nex_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com',"test1@example.com"],
+            ['test2@EXAMPLE.Com',"test2@example.com"],
+        ]
+
+        for email,excepted in sample_emails:
+            user = get_user_model().objects.create_user(email,"sample123")
+            self.assertEqual(user.email,excepted)
+
+
+    def test_new_user_without_email_raises_error(self):
+        with  self.assertRaises(ValueError):
+            get_user_model().objects.create_user("","test123")
+
