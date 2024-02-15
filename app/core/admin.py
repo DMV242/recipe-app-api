20513b78747be69409f3a80d72b2c2b7c25f69f5
MMEDV242 <mvouladavid@gmail.com>
2024-02-15 16:10:18 +0100

    change all app code

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,60 +1,50 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
-from django.contrib.admin import AdminSite
-
-# Register your models here.
-
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
-
-
-
-    ordering = ["-id"]
-    list_display = ["email","name","is_active"]
-
-    list_editable = ["is_active"]
-
-
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
     fieldsets = (
-        (None,{"fields":("email", "password")}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-            _("Permissions"),
+            _('Permissions'),
             {
-                "fields":(
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
                 )
             }
         ),
-        (_("Important dates"),{ "fields":("last_login",)}),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ["last_login"]
-
+    readonly_fields = ['last_login']
     add_fieldsets = (
-        (None, {"classes": ("wide",),
-                "fields":(
-                    "email",
-                    "password1",
-                    "password2",
-                    "name",
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    )
-                }),
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
     )
 
 
-
-
-
-
-admin.site.site_header = "Recipe app Administration"
-admin.site.register(models.User,UserAdmin)
+admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
--- app/user/views.py
+++ app/user/views.py
@@ -1,25 +1,22 @@
 """
-View for users api
+Views for the user API.
 """
-from rest_framework import generics,authentication,permissions
+from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from user.serializers import UserSerializer,AuthTokenSerializer
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
     serializer_class = UserSerializer
+
 
 class CreateTokenView(ObtainAuthToken):
-   serializer_class = AuthTokenSerializer
-   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
-class ManageUserView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
-    permission_classes =[permissions.IsAuthenticated]
-    authentication_classes = [authentication.TokenAuthentication]
-
-
-    def get_object(self):
-        return self.request.user
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
