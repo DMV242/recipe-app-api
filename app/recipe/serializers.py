d50140dbb8b844cc6baf7ecff60793c430971615
MMEDV242 <mvouladavid@gmail.com>
2024-02-17 16:51:06 +0100

    Update settings.py, recipe/serializers.py, recipe/tests/test_recipe_api.py, recipe/views.py

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,10 +3,13 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
+
+
 from core.models import (
     Recipe,
     Tag,
     Ingredient,
+
 )
 
 
@@ -46,7 +49,9 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description',"image"]
+
+
 
     def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed."""
@@ -94,3 +99,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
         instance.save()
         return instance
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer fro uploading image tp recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ["id","image"]
+        read_only_fields = ["id"]
+        extra_kwargs = {"image":{"required":"True"}}
